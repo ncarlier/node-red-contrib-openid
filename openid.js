ea29ab8ae122769d20d1058a46a3553b1c26591b
Alex S Kaye <noxdeleo@googlemail.com>
2022-02-09 17:14:11 +0000

    Upgrade openid-client to 5.x.x and fix API calls for new version

--- openid.js
+++ openid.js
@@ -90,8 +90,8 @@ module.exports = function (RED) {
 
     Issuer.discover(credentials.discovery_url).then(issuer => {
       const client = new issuer.Client(credentials)
-      client.authorizationCallback(credentials.redirect_uri, {code: req.query.code}).then((tokenSet) => {
-        const claims = tokenSet.claims
+      client.callback(credentials.redirect_uri, {code: req.query.code}).then((tokenSet) => {
+        const claims = tokenSet.claims()
         RED.nodes.addCredentials(node_id, Object.assign({}, credentials, {
           id_token: tokenSet.id_token,
           refresh_token: tokenSet.refresh_token,
